c5f351fc4a590349a833d588039f1dd311c44d81
Bharti <110337668+Bharti025@users.noreply.github.com>
2025-09-16 13:53:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,23 +3,37 @@ let title=document.getElementById("title");
 let author=document.getElementById("author");
 let isbn=document.getElementById("isbn");
 let button=document.getElementById("submit");
-let table=document.getElementsByClassName("table table-striped");
+let tableBody=document.getElementById("book-list");
+
 button.addEventListener("click",()=>{
-title=title.value;
-author=author.value;
-isbn=isbn.value;
+
 	
 td1=document.createElement("td");
-td1.append(title);
+td1.append(title.value);
 td2=document.createElement("td");
-td2.append(author);
+td2.append(author.value);
 td3=document.createElement("td");
-td3.append(isbn);
+td3.append(isbn.value);
+	let td4 = document.createElement("td");
+    
+    // Create delete button
+    let deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete";
+    deleteButton.className = "delete";
+    deleteButton.addEventListener("click", () => {
+        row.remove(); // Remove the row when delete button is clicked
+    });
+    td4.append(deleteButton);
+
 let row=document.createElement("tr");
 	row.append(td1);
 	row.append(td2);
 	row.append(td3);
-   table[0].append(row);	
+	row.append(td4);
+   tableBody.append(row);
+	title.value="";
+	author.value="";
+	isbn.value="";
 })
 
 
